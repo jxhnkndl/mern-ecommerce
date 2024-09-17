d59518e131b39eddadc74e6321e29b866d57f437
jxhnkndl <jkroyston@gmail.com>
2024-09-17 16:29:08 -0400

    convert products.js as seed data that matches the product model and create user seed data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'J.K. Royston',
+    email: 'jk@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Lou Keeton',
+    email: 'lou@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  }
+];
+
+export default users;
