1feef9d6b9299cbb38d4475f0f65b3c1bdccb369
jxhnkndl <jkroyston@gmail.com>
2024-09-17 14:31:07 -0400

    update components to use Link and LinkContainer components for navigation functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
@@ -9,21 +10,28 @@ const Header = () => {
       {/* Navbar collapses at large breakpoint and has dark theme */}
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} alt="ProShop" />
-            ProShop
-          </Navbar.Brand>
+          {/* This is a react-router-bootstrap component for wrapping blocks in React friendly links */}
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt='ProShop' />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           {/* Hamburger icon */}
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           {/* Collapsible hamburger menu */}
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/card'>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 const Product = ({ product }) => {
   return (
+    // Links navigate to this specific product's details
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product.id}`}>
+      <Link to={`/product/${product.id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product.id}`}>
+        <Link to={`/product/${product.id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="h3">
           ${product.price}
         </Card.Text>
