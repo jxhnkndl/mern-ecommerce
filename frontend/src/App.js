10744f5995bc46f7991b2ac9fe7c1f5e8e6a5042
jxhnkndl <jkroyston@gmail.com>
2024-09-17 14:30:51 -0400

    init core react router and install outlet in app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -10,7 +10,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          {/* This is where interior routes get rendered */}
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+
+// Create routes
+const router = createBrowserRouter(createRoutesFromElements(
+  // This is the master component that gets render when user visits /
+  // Interior routes get rendered inside the <Outlet /> component in <App />
+  // The true index ensures that ONLY the home screen shows up at the / route
+  <Route path="/" element={<App />}>
+    <Route index={true} path="/" element={<HomeScreen />} />
+  </Route>
+));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Render the RouterProvider since that's the structure rendering <App /> and its screens */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
