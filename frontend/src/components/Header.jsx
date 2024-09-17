cd8070b70391ec52986d69d498a5b1f2d6f958f6
jxhnkndl <jkroyston@gmail.com>
2024-09-17 13:54:36 -0400

    create header and footer components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Navbar collapses at large breakpoint and has dark theme */}
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          {/* Hamburger icon */}
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          {/* Collapsible hamburger menu */}
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/card'>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
