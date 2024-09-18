72b9112dffb780a612a8b215382eb28a5cb6ace2
jxhnkndl <jkroyston@gmail.com>
2024-09-18 16:12:18 -0400

    show total number of items in cart in header next to cart icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+  // Extract quatity value from cart state to show how many items are in cart in header
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       {/* Navbar collapses at large breakpoint and has dark theme */}
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
           {/* This is a react-router-bootstrap component for wrapping blocks in React friendly links */}
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>
               <img src={logo} alt='ProShop' />
               ProShop
@@ -22,12 +26,20 @@ const Header = () => {
           {/* Collapsible hamburger menu */}
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to="/cart">
+              <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                      <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                        {/* Add up all the items in the cart (sum up quantity of each item) */}
+                        {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                      </Badge>
+                    )
+                  }
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaUser /> Sign In
                 </Nav.Link>
