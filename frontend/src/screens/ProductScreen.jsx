473ec7fb07491960a9734db31341d83dac2617df
jxhnkndl <jkroyston@gmail.com>
2024-09-18 16:01:57 -0400

    add functionality for adding items to cart, choosing a quantity, dispatching the action to the reducer, and navigating to cart

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Form,
+  Button,
+} from 'react-bootstrap';
 import { useGetProductDetailsQuery } from '../slices/productsSlice';
+import { addToCart } from '../slices/cartSlice';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const ProductScreen = () => {
+  const [qty, setQty] = useState(1);
+
   // Capture product's id from URL params
   const { id: productId } = useParams();
+
+  // Init navigation object
+  const navigate = useNavigate();
+
+  // Init dispatch method
+  const dispatch = useDispatch();
 
   // Fetch product details from API through Redux's API slice using the product's ID in the endpoint
   const {
@@ -16,6 +34,13 @@ const ProductScreen = () => {
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  // Dispatch the product pulled in from the API along with the chosen quantity
+  // Navigate to cart after adding product to cart
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate('/cart');
+  };
 
   return (
     <>
@@ -25,7 +50,7 @@ const ProductScreen = () => {
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">
+        <Message variant='danger'>
           {error?.data?.message || error.error}
         </Message>
       ) : (
@@ -72,11 +97,37 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(Number(e.target.value))}>
+                          {/* Create a array with the same number of elements as there are items in stock */}
+                          {/* EX: If there are 10 items in stock, the array will have a .length of 10 */}
+                          {/* .keys() creates an array of indexes (starting at 0) */}
+                          {[...Array(product.countInStock).keys()].map(
+                            (num) => (
+                              // Adding 1 so that the qty doesn't start at 0 where the array values begin
+                              <option key={num + 1} value={num + 1}>
+                                {num + 1}
+                              </option>
+                            )
+                          )}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   {/* Render button disabled if product is not in stock */}
                   <Button
                     className='btn-block'
                     type='button'
+                    onClick={addToCartHandler}
                     disabled={product.countInStock === 0}>
                     Add to Cart
                   </Button>
