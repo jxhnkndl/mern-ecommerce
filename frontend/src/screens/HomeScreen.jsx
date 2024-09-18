d272e007caa68648a3df84b65699c2f1f5289627
jxhnkndl <jkroyston@gmail.com>
2024-09-18 13:12:58 -0400

    update screens to fetch products and single product details from API via API slices

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,34 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsSlice';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    }
+  // Fetch data from API and extract key data and state properties from response
+  // Function and endpoint are defined in the productsSlice.js script
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-    fetchProducts();
-  }, []);
-  
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          // Small Screens: Col spans 12 column units
-          // Medium Screens: Col spans 6 column units
-          // Large Screens: Col spans 4 column units
-          // XL Screens: Col spans 2 column units
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              // Small Screens: Col spans 12 column units
+              // Medium Screens: Col spans 6 column units
+              // Large Screens: Col spans 4 column units
+              // XL Screens: Col spans 2 column units
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
+
+      
     </>
   );
 };
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,82 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import axios from 'axios';
+import { useGetProductDetailsQuery } from '../slices/productsSlice';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
-
   // Capture product's id from URL params
   const { id: productId } = useParams();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios(`/api/products/${productId}`);
-      setProduct(data);
-    }
-
-    fetchProduct();
-
-    // Rerun useEffect if productId changes
-  }, [productId]);
+  // Fetch product details from API through Redux's API slice using the product's ID in the endpoint
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link to='/' className='btn btn-light my-3'>
         Go Back
       </Link>
-      <Row>
-        {/* Image Column */}
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        {/* Product Details Column */}
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.rating} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>{product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        {/* Price + Purchase Column */}
-        <Col md={3}>
-          <Card>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Row>
+          {/* Image Column */}
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          {/* Product Details Column */}
+          <Col md={4}>
             <ListGroup variant='flush'>
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.rating} reviews`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item>
-                {/* Render button disabled if product is not in stock */}
-                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
-                  Add to Cart
-                </Button>
-              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          {/* Price + Purchase Column */}
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {/* Render button disabled if product is not in stock */}
+                  <Button
+                    className='btn-block'
+                    type='button'
+                    disabled={product.countInStock === 0}>
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
