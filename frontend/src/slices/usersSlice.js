79aa8d08e3b8e54be62007a818ccabd94c6ae1f8
jxhnkndl <jkroyston@gmail.com>
2024-09-20 15:06:25 -0400

    add registration mutation to user redux slice

--- frontend/src/slices/usersSlice.js
+++ frontend/src/slices/usersSlice.js
@@ -11,6 +11,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/`,
+        method: 'POST',
+        body: data
+      })
+    }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
@@ -21,4 +28,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 });
 
 // Export actions
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } = usersApiSlice;
