7c6d457cefe22cf5c2c1dabb830355d6b4ba0162
jxhnkndl <jkroyston@gmail.com>
2024-09-20 13:40:48 -0400

    add functionality for getting and updating user profiles

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
     // Create JWT for authentication
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -80,14 +80,53 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Get user profile');
+  const user = await User.findById(req.user._id);
+
+  // If user exists, respond with user profile data
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('Update user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    // Check to see if property was edited
+    // If so, keep new value; if not, keep existing value
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    // Check password separately since it's hashed
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    // Save changes to user
+    const updatedUser = await user.save();
+
+    // Respond to client with updated user data
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin
+    }); 
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Get all users
