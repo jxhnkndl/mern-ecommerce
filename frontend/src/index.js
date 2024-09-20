6c2df24ae5eb1c05e80b1ef7fa93b3ea53d7af0e
jxhnkndl <jkroyston@gmail.com>
2024-09-20 09:31:28 -0400

    create cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,20 +11,23 @@ import store from './store';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import reportWebVitals from './reportWebVitals';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 
-// Create routes
-// This is the master component that gets render when user visits /
+// This is the master router component that gets rendered when user visits /
 // Interior routes get rendered inside the <Outlet /> component in <App />
 // The true index ensures that ONLY the home screen shows up at the / route
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index={true} path="/" element={<HomeScreen />} />
-    <Route path="/product/:id" element={<ProductScreen />} />
-  </Route>
-));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
