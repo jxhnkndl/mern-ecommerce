81a619eccb44cae3d797534ee00b0103d22f496c
jxhnkndl <jkroyston@gmail.com>
2024-09-23 13:12:54 -0400

    create shipping screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import reportWebVitals from './reportWebVitals';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
